258fbd03ec2a4099c31723d9b2b1cb13e0e9161d
xiaoshangmin <shangminxiao@gmail.com>
2021-03-13 12:27:51 +0800

    update

---
+++ app/admin/lang/zh-cn/ns/top_config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Price'       => '价格',
+    'Type'        => '期限类型',
+    'Type 1'      => '一天',
+    'Type 2'      => '一周',
+    'Type 3'      => '一个月',
+    'Sort'        => '排序',
+    'Create_time' => '创建时间',
+    'Update_time' => '更新时间'
+];
---
+++ app/admin/model/ns/TopConfig.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\admin\model\ns;
+
+use app\common\model\BaseModel;
+
+
+class TopConfig extends BaseModel
+{
+
+    
+
+    
+
+    // 表名
+    protected $name = 'top_config';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+        'type_text',
+        'create_time_text',
+        'update_time_text'
+    ];
+    
+
+    
+    public function getTypeList()
+    {
+        return ['1' => __('Type 1'), '2' => __('Type 2'), '3' => __('Type 3')];
+    }
+
+
+    public function getTypeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['type']) ? $data['type'] : '');
+        $list = $this->getTypeList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+
+    public function getCreateTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['create_time']) ? $data['create_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getUpdateTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['update_time']) ? $data['update_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+    protected function setCreateTimeAttr($value)
+    {
+        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
+    }
+
+    protected function setUpdateTimeAttr($value)
+    {
+        return $value === '' ? null : ($value && !is_numeric($value) ? strtotime($value) : $value);
+    }
+
+
+}
